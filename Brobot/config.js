0d1665a5707287bd8ba680df970af6471082735f
Jeff Jassky <jeff@jeffjassky.com>
2016-06-20 22:34:01 -0400

    Assigned pins to standard midi notes

--- Brobot/config.js
+++ Brobot/config.js
@@ -1,24 +1,45 @@
 var config = {
 	instruments: [
 		{
-			name: "snare",
+			name: "(electric) Snare",
+			note: 40,
+			pinNumber: 9,
+			softVelocity: 20,
+			softLatencyAbsolute: 50
+		},
+		{
+			name: "(electric) Kick",
 			note: 36,
 			pinNumber: 5,
+			softVelocity: 40,
+			softLatencyAbsolute: 50
+		},
+		{
+			name: "(closed) Hat",
+			note: 42,
+			pinNumber: 10,
 			softVelocity: 20,
 			softLatencyAbsolute: 50
 		},
 		{
-			name: "kick",
-			note: 43,
+			name: "Crash (cymbal 1)",
+			note: 49,
+			pinNumber: 11,
+			softVelocity: 20,
+			softLatencyAbsolute: 50
+		},
+		{
+			name: "Floor Tom (low)",
+			note: 41,
 			pinNumber: 6,
-			softVelocity: 20,
+			softVelocity: 50,
 			softLatencyAbsolute: 50
 		},
 		{
-			name: "Hat",
-			note: 29,
-			pinNumber: 8,
-			softVelocity: 20,
+			name: "Low Tom",
+			note: 45,
+			pinNumber: 7,
+			softVelocity: 50,
 			softLatencyAbsolute: 50
 		}
 	]
